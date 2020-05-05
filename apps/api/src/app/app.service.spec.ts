df02afbb531647533f8d0fa83cbd581c05f1dd12
Fábio Rito <fabioaarito@gmail.com>
2020-05-05 16:04:08 +0100

    fix tests

--- apps/api/src/app/app.service.spec.ts
+++ apps/api/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Welcome to api!"', () => {
-      expect(service.getData()).toEqual({ message: 'Welcome to api!' });
+    it('should return "Welcome to Share humanity"', () => {
+      expect(service.getData()).toEqual({ message: 'Welcome to Share humanity' });
     });
   });
 });
